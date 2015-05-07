351158d19dad538a33bcd30d72d93255810b33c0
Karthik Krishnan <karthik@Karthiks-MacBook-Pro.local>
2015-05-07 10:16:51 -0700

    Sort AMIs by CreationDate

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Prints AMI Mapping table to be used in Cloudformation templates. Uses node for parsing and aws cli for ami query.
+AMIs are sorted by CreationDate and latest one is chosen
+Change ami names or use RegEx if needed.
+
 Usage: node aws-ami.js
 
-Change ami names or use RegEx if needed.
--- aws-ami.js
+++ aws-ami.js
@@ -1,6 +1,6 @@
 var child_process = require('child_process');
-var amiNATName="amzn-ami-vpc-nat-hvm-2015.03.0.x86_64-gp2"
-var amiAMZN64HVM="amzn-ami-hvm-2015.03.0.x86_64-gp2"
+var amiNATName="amzn-ami-vpc-nat-hvm-*x86_64-gp2"
+var amiAMZN64HVM="amzn-ami-hvm-*x86_64-gp2"
 
 if (!String.prototype.contains) {
     String.prototype.contains = function (arg) {
@@ -31,7 +31,10 @@ function getAMIbyName(name,region) {
   var ami = child_process.execSync(cmd, { encoding: 'utf8' });
 
   return JSON.parse(ami);
+}
 
+function CompareCreationDate(a,b) {
+	return Date.parse(a.CreationDate) - Date.parse(b.CreationDate) 
 }
 
 
@@ -42,10 +45,12 @@ function printNATAMI() {
   for (v in regions.Regions) {
     var _region = (regions.Regions[v].RegionName);
     var ami = getAMIbyName(amiNATName,_region);
+    var amiArray = Object.keys(ami.Images).map(function (key) {return ami.Images[key]});
+    var amiArraySorted = amiArray.sort(CompareCreationDate);
 
     amis.AWSNATAMI[_region] = {};
     //pick the first one if many
-    amis.AWSNATAMI[_region]["AMI"] = ami.Images[0].ImageId;
+    amis.AWSNATAMI[_region]["AMI"] = amiArraySorted[amiArraySorted.length-1].ImageId;
   }
 
   console.log(JSON.stringify(amis, null, 4));
@@ -58,10 +63,12 @@ function printAMZNAMI() {
   for (v in regions.Regions) {
     var _region = (regions.Regions[v].RegionName);
     var ami = getAMIbyName(amiAMZN64HVM,_region);
+    var amiArray = Object.keys(ami.Images).map(function (key) {return ami.Images[key]});
+    var amiArraySorted = amiArray.sort(CompareCreationDate);
 
     amis.AMI[_region] = {};
-    //pick the first one if many
-    amis.AMI[_region]["64HVM"] = ami.Images[0].ImageId;
+    //pick the first one, sorted by date
+    amis.AMI[_region]["64HVM"] = amiArraySorted[amiArraySorted.length-1].ImageId;
   }
 
   console.log(JSON.stringify(amis, null, 4));
